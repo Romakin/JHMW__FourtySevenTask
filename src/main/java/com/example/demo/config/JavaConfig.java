5f068a66b65e58ee633b4d13579cfe27aecd710e
Serg <romakinsv@gmail.com>
2020-12-20 20:41:18 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/config/JavaConfig.java
@@ -0,0 +1,24 @@
+package com.example.demo.config;
+
+import com.example.demo.profile.DevProfile;
+import com.example.demo.profile.ProductionProfile;
+import com.example.demo.profile.SystemProfile;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JavaConfig {
+
+    @Bean
+    @ConditionalOnProperty(prefix = "netology.profile", name = "dev", havingValue = "false", matchIfMissing = true)
+    public SystemProfile devProfile() {
+        return new DevProfile();
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = "netology.profile", name = "dev", havingValue = "true")
+    public SystemProfile prodProfile() {
+        return new ProductionProfile();
+    }
+}
